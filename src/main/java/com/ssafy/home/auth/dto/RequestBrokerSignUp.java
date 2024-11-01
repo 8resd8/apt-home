581c25d976729f8dbec7054c31a0f2253a8841dc
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-02 02:02:22 +0900

    feat: 메시지 공통화 구현

--- src/main/java/com/ssafy/home/auth/dto/RequestBrokerSignUp.java
+++ src/main/java/com/ssafy/home/auth/dto/RequestBrokerSignUp.java
@@ -6,36 +6,35 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RequestBrokerSignUp(
-        @NotBlank(message = "아이디는 필수 항목입니다.")
-        @Size(min = 5, max = 255, message = "아이디는 5자 이상 255자 이하로 입력해야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(min = 5, max = 255, message = "{size.id}")
         String id,
 
-        @NotBlank(message = "비밀번호는 필수 항목입니다.")
-//        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(min = 8, message = "{size.password}")
         String password,
 
-        @NotBlank(message = "중개사 이름은 필수 항목입니다.")
-        @Size(max = 45, message = "중개사 이름은 최대 45자까지 입력할 수 있습니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(max = 45, message = "{size.max}")
         String officeName,
 
-        @NotBlank(message = "이름은 필수 항목입니다.")
-        @Size(max = 45, message = "이름은 최대 45자까지 입력할 수 있습니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(max = 45, message = "{size.max}")
         String name,
 
-        @NotBlank(message = "전화번호는 필수 항목입니다.")
-        @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호는 000-0000-0000 형식이어야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "{pattern.phoneNum}")
         String phoneNum,
 
-        @NotBlank(message = "주소는 필수 항목입니다.")
-        @Size(max = 45, message = "주소는 최대 45자까지 입력할 수 있습니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(max = 45, message = "{size.max}")
         String address,
 
-        @NotBlank(message = "자격증 번호는 필수 항목입니다.")
-        @Size(max = 45, message = "자격증 번호는 최대 45자까지 입력할 수 있습니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(max = 45, message = "{size.max}")
         String licenseNum,
 
-        @NotBlank(message = "이메일은 필수 항목입니다.")
-        @Email(message = "유효한 이메일 주소를 입력해야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Email(message = "{email.valid}")
         String email
-) {
-}
+) {}
--- src/main/java/com/ssafy/home/auth/dto/RequestMemberSignUp.java
+++ src/main/java/com/ssafy/home/auth/dto/RequestMemberSignUp.java
@@ -5,24 +5,23 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-
 public record RequestMemberSignUp(
-        @NotBlank(message = "아이디는 필수 항목입니다.")
-        @Size(min = 5, max = 255, message = "아이디는 5자 이상 255자 이하로 입력해야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(min = 5, max = 255, message = "{size.id}")
         String id,
 
-        @NotBlank(message = "비밀번호는 필수 항목입니다.")
-//        @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(min = 8, message = "{size.password}")
         String password,
 
-        @NotBlank(message = "이메일은 필수 항목입니다.")
-        @Email(message = "유효한 이메일 주소를 입력해야 합니다.")
+        @NotBlank(message = "{required.field}")
+        @Email(message = "{email.valid}")
         String email,
 
-        @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호는 000-0000-0000 형식이어야 합니다.")
+        @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "{pattern.phoneNum}")
         String phoneNum,
 
-        @NotBlank(message = "이름은 필수 항목입니다.")
-        @Size(max = 45, message = "이름은 최대 45자까지 입력할 수 있습니다.")
+        @NotBlank(message = "{required.field}")
+        @Size(max = 45, message = "{size.max}")
         String name
-) { }
+) {}
