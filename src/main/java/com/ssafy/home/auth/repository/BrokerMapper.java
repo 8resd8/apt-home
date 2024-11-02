99db5e5fdcf651a290f05736b8281da22f548fd6
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-02 15:19:11 +0900

    refactor: email 필드 추가

--- src/main/java/com/ssafy/home/auth/repository/BrokerMapper.java
+++ src/main/java/com/ssafy/home/auth/repository/BrokerMapper.java
@@ -1,9 +1,11 @@
 package com.ssafy.home.auth.repository;
 
+import com.ssafy.home.auth.domain.Broker;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Mapper
 public interface BrokerMapper {
@@ -16,7 +18,10 @@ public interface BrokerMapper {
                       @Param("licenseNum") String licenseNum,
                       @Param("password") String password,
                       @Param("salt") String salt,
+                      @Param("email") String email,
                       @Param("lastLogin") LocalDateTime lastLogin,
                       @Param("createdAt") LocalDateTime createdAt,
                       @Param("updatedAt") LocalDateTime updatedAt);
+
+    Optional<Broker> findById(@Param("id") String id);
 }
