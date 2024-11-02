645390672339c5ddd74d56eca77d2e67bd2e5a3e
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-02 20:36:26 +0900

    feat: 커스텀 애노테이션 등록

--- src/main/java/com/ssafy/home/config/WebConfig.java
+++ src/main/java/com/ssafy/home/config/WebConfig.java
@@ -1,16 +1,21 @@
 package com.ssafy.home.config;
 
 import com.ssafy.home.interceptor.AuthInterceptor;
+import com.ssafy.home.interceptor.AuthLoginResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final AuthLoginResolver loginUserArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -18,5 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/login", "/signup", "/", "/error", "/css/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver); // 커스텀 Argument Resolver 등록
     }
 }
