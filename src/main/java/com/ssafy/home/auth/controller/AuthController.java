4a9f2fe1f59966b93ad64962f958f841d7e49603
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-04 21:06:55 +0900

    refactor: 로그아웃 Get 방식 변경

--- src/main/java/com/ssafy/home/auth/controller/AuthController.java
+++ src/main/java/com/ssafy/home/auth/controller/AuthController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -39,7 +36,7 @@ public class AuthController {
         return new ResponseEntity<>(responseLogin, HttpStatus.CREATED);
     }
 
-    @PostMapping("/logout")
+    @GetMapping("/logout")
     public ResponseEntity<String> logout() {
         authService.logout();
         return ResponseEntity.ok("로그아웃 되었습니다.");
