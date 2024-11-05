f8c59953f08046b9a9796b5100bd2be37fd02c61
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-05 21:26:22 +0900

    feat: 쓰지않는 메서드 제거 및 인터셉터 확인 경로만 남김

--- src/test/java/com/ssafy/home/testcontroller/TestController.java
+++ src/test/java/com/ssafy/home/testcontroller/TestController.java
@@ -1,26 +1,11 @@
 package com.ssafy.home.testcontroller;
 
-import com.ssafy.home.annotation.Login;
-import com.ssafy.home.auth.domain.Broker;
-import com.ssafy.home.auth.domain.Member;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // 테스트에 사용할 컨트롤러
 @RestController
 public class TestController {
-
-    @GetMapping("/member")
-    public String getMember(@Login Member member) {
-        return member != null ? "Member: " + member.getMid() : "No member";
-    }
-
-    @GetMapping("/broker")
-    public String getBroker(@Login Broker broker) {
-        return broker != null ? "Broker: " + broker.getBid() : "No broker";
-    }
 
     @PostMapping("/broker/some-path")
     public String testBroker() {
