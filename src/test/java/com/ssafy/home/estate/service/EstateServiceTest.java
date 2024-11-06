47eb7b7b8bccb0688e55155bfba099ab5dab89dd
김현우 <dkanfjgwls@naver.com>
2024-11-07 01:47:57 +0900

    test: estateService 테스트코드 추가
    매물 등록과 매물 상세 조회 메소드에 대해 정상 작동 테스트 코드 작성

---
+++ src/test/java/com/ssafy/home/estate/service/EstateServiceTest.java
@@ -0,0 +1,105 @@
+package com.ssafy.home.estate.service;
+
+import com.ssafy.home.auth.domain.Broker;
+import com.ssafy.home.auth.dto.BrokerSignUpRequest;
+import com.ssafy.home.auth.repository.BrokerMapper;
+import com.ssafy.home.auth.service.AuthService;
+import com.ssafy.home.estate.dto.EstateDetailResponseDto;
+import com.ssafy.home.estate.dto.RegistEstateRequestDto;
+import com.ssafy.home.global.enums.estateType;
+import com.ssafy.home.global.repository.UtilMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class EstateServiceTest {
+
+    @Autowired
+    private EstateService estateService;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private BrokerMapper brokerMapper;
+
+    @Autowired
+    private UtilMapper utilMapper;
+
+    private Broker broker;
+    private String brokerId = "broker1";
+
+
+    @BeforeEach
+    void setUp() {
+        BrokerSignUpRequest brokerSignUpRequest = new BrokerSignUpRequest(
+                brokerId,
+                "password456",
+                "Test Office",
+                "Jane Doe",
+                "010-8765-4321",
+                "Seoul",
+                "123-45-6789",
+                "broker@test.com"
+        );
+
+        authService.signUpBroker(brokerSignUpRequest);
+
+        broker = brokerMapper.findById(brokerId).get();
+    }
+
+    @DisplayName("매물 정상 등록")
+    @Test
+    public void postEstate() {
+        //given
+        RegistEstateRequestDto requestDto = new RegistEstateRequestDto(
+                "11110-100",
+                estateType.매매,
+                "200000",
+                3,
+                7,
+                5.4,
+                "테스트 매물입니다."
+        );
+
+        //when
+        estateService.createEstate(requestDto, broker);
+        Long createdId = utilMapper.selectLastInsertId();
+        //then
+
+        Assertions.assertNotNull(createdId);
+        Assertions.assertNotNull(estateService.findEstateById(createdId));
+    }
+
+    @DisplayName("매물 상세 조회")
+    @Test
+    public void getEstate() {
+        //given
+        RegistEstateRequestDto requestDto = new RegistEstateRequestDto(
+                "11110-100",
+                estateType.매매,
+                "200000",
+                3,
+                7,
+                5.4,
+                "테스트 매물입니다."
+        );
+        estateService.createEstate(requestDto, broker);
+        Long createdId = utilMapper.selectLastInsertId();
+
+        //when
+        EstateDetailResponseDto responseDto = estateService.findEstateDetailById(createdId);
+
+        //then
+        Assertions.assertNotNull(responseDto);
+    }
+}
+
+
