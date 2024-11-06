0bb7860278ccce88e75ee2c06acab2bd368a62f6
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-06 23:54:12 +0900

    feat: 찜 목록 추가, 삭제, 전체조회 구현

---
+++ src/main/java/com/ssafy/home/favorite/service/FavoriteServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ssafy.home.favorite.service;
+
+
+import com.ssafy.home.favorite.domain.Favorite;
+import com.ssafy.home.favorite.dto.FavoriteAddRequest;
+import com.ssafy.home.favorite.exception.DuplicateFavoriteException;
+import com.ssafy.home.favorite.repository.FavoriteMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class FavoriteServiceImpl implements FavoriteService {
+
+    private final FavoriteMapper favoriteMapper;
+
+    @Override
+    public void addFavorite(FavoriteAddRequest request) {
+        if (favoriteMapper.findFavorite(request.memberId(), request.estateId()).isPresent()) {
+            throw new DuplicateFavoriteException("이미 찜 목록에 존재하는 항목입니다.");
+        }
+
+        favoriteMapper.insertFavorite(request.memberId(), request.estateId());
+    }
+
+    @Override
+    public void removeFavorite(FavoriteAddRequest request) {
+        favoriteMapper.deleteFavorite(request.memberId(), request.estateId());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Favorite> getFavoriteAll(String memberId) {
+        return favoriteMapper.findAllFavoriteByMemberId(memberId);
+    }
+}
