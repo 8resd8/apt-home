894e680f18715c2e1834a10e8efe554075c8691a
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-06 23:54:39 +0900

    feat: 찜 목록 추가, 삭제, 전체조회 매퍼 작성

---
+++ src/main/java/com/ssafy/home/favorite/repository/FavoriteMapper.java
@@ -0,0 +1,22 @@
+package com.ssafy.home.favorite.repository;
+
+import com.ssafy.home.favorite.domain.Favorite;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface FavoriteMapper {
+
+    int insertFavorite(@Param("memberId") String memberId, @Param("estateId") Long estateId);
+
+    int deleteFavorite(@Param("memberId") String memberId, @Param("estateId") Long estateId);
+
+    List<Favorite> findAllFavoriteByMemberId(@Param("memberId") String memberId);
+
+    Optional<Favorite> findFavorite(@Param("memberId") String memberId, @Param("estateId") Long estateId);
+
+
+}
