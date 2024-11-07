efeddccac2258e010f620a8c7a7fbae4f7968e1e
김현우 <dkanfjgwls@naver.com>
2024-11-07 17:16:13 +0900

    feat: 로그인 여부만 확인하는 인터셉터로 구분

---
+++ src/main/java/com/ssafy/home/global/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package com.ssafy.home.global.interceptor;
+
+import com.ssafy.home.global.enums.Session;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute(Session.TYPE.name()) == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
+            return false;
+        }
+
+        return true;
+    }
+}
