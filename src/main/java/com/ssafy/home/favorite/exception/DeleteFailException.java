8427d7a2d456bd041e20749b1d4335eba1142bf1
김현우 <dkanfjgwls@naver.com>
2024-11-07 15:25:31 +0900

    feat: 삭제 실패 예외구현

---
+++ src/main/java/com/ssafy/home/favorite/exception/DeleteFailException.java
@@ -0,0 +1,7 @@
+package com.ssafy.home.favorite.exception;
+
+public class DeleteFailException extends RuntimeException {
+    public DeleteFailException(String message) {
+        super(message);
+    }
+}
