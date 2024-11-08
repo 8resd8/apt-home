c8d740828bea431233387a178c50828cf21c3dca
김현우 <dkanfjgwls@naver.com>
2024-11-08 15:04:38 +0900

    refactor: estateManageController 분리

--- src/main/java/com/ssafy/home/estate/controller/estateController.java
+++ src/main/java/com/ssafy/home/estate/controller/estateController.java
@@ -1,14 +1,10 @@
 package com.ssafy.home.estate.controller;
 
-import com.ssafy.home.global.annotation.Login;
-import com.ssafy.home.auth.domain.Broker;
 import com.ssafy.home.estate.dto.EstateDetailResponseDto;
-import com.ssafy.home.estate.dto.RegistEstateRequestDto;
 import com.ssafy.home.estate.service.EstateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -19,14 +15,7 @@ public class estateController {
 
     private final EstateService estateService;
 
-    @PostMapping
-    public ResponseEntity<?> postEstate(@Validated @RequestBody RegistEstateRequestDto requestDto, @Login Broker broker) {
-        Long createdId = estateService.createEstate(requestDto, broker);
-
-        return ResponseEntity.ok().body(createdId);
-    }
-
-    @GetMapping("/view/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<EstateDetailResponseDto> getEstate(@PathVariable Long id) {
         EstateDetailResponseDto responseDto = estateService.findEstateDetailById(id);
 
