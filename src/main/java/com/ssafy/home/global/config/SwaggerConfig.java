88cfb2f3a81b2575090cd8a1e437f8fc593fd552
김현우 <dkanfjgwls@naver.com>
2024-11-08 15:38:39 +0900

    chore: clean up unused import s

--- src/main/java/com/ssafy/home/global/config/SwaggerConfig.java
+++ src/main/java/com/ssafy/home/global/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.ssafy.home.global.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
