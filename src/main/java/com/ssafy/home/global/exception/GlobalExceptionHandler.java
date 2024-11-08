2c4a6a47676aaf07ca2a31c6312a66ab156b11f9
김현우 <dkanfjgwls@naver.com>
2024-11-08 17:45:47 +0900

    feat: NOT FOUND Exception

--- src/main/java/com/ssafy/home/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/home/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ssafy.home.global.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import org.springframework.http.ResponseEntity;
@@ -7,10 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { CustomException.class })
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         return ErrorResponse.toResponseEntity(e.getStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    protected ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
+        return ErrorResponse.toResponseEntity(HttpStatus.NOT_FOUND, e.getMessage());
     }
 }
