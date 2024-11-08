ecf23c47aa0264127e47910dad04c3b875bbd929
김현우 <dkanfjgwls@naver.com>
2024-11-08 16:46:29 +0900

    feat: 예약 생성하기 수정, 예약 테이블 생성 인터페이스

--- src/main/java/com/ssafy/home/reservation/service/ReservationService.java
+++ src/main/java/com/ssafy/home/reservation/service/ReservationService.java
@@ -1,18 +1,20 @@
 package com.ssafy.home.reservation.service;
 
 import com.ssafy.home.reservation.domain.Reservation;
-import com.ssafy.home.reservation.dto.ReservationAddRequest;
+import com.ssafy.home.reservation.dto.ReservationCreateRequest;
 
 import java.util.List;
 
 public interface ReservationService {
-    void addReservation(ReservationAddRequest request);
-
-    void updateReservation(Long rid, ReservationAddRequest request);
+    void updateReservation(Long rid, ReservationCreateRequest request, String memberId);
 
     void deleteReservation(Long rid);
 
     Reservation getReservation(Long rid);
 
-    List<Reservation> getReservationsByMember(String memberId);
+    List<Reservation> getAllReservationByMember(String memberId);
+
+    void createReservation(ReservationCreateRequest request, String memberId);
+
+
 }
