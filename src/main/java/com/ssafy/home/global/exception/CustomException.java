92a6a0665a15fdcde6332d1f1a360c63cb1e78fa
김현우 <dkanfjgwls@naver.com>
2024-11-08 17:20:12 +0900

    feat: ExceptionHandler 등록

---
+++ src/main/java/com/ssafy/home/global/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.ssafy.home.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException{
+    private final HttpStatus status;
+    private final String message;
+}
