1a00927e76d7acdbd648081e5c3427facfcc545f
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-09 21:01:11 +0900

    refactor: 변경된 요청에 따른 수정

--- src/main/java/com/ssafy/home/favorite/service/FavoriteService.java
+++ src/main/java/com/ssafy/home/favorite/service/FavoriteService.java
@@ -1,5 +1,6 @@
 package com.ssafy.home.favorite.service;
 
+import com.ssafy.home.auth.domain.Member;
 import com.ssafy.home.favorite.domain.Favorite;
 import com.ssafy.home.favorite.dto.FavoriteAddRequest;
 import com.ssafy.home.favorite.dto.FavoriteResponse;
@@ -7,9 +8,9 @@ import com.ssafy.home.favorite.dto.FavoriteResponse;
 import java.util.List;
 
 public interface FavoriteService {
-    void addFavorite(FavoriteAddRequest request);
+    void addFavorite(Member member, FavoriteAddRequest request);
 
-    void removeFavorite(FavoriteAddRequest request);
+    void removeFavorite(Member member, FavoriteAddRequest request);
 
     List<FavoriteResponse> getFavoriteAll(String memberId);
 }
--- src/main/java/com/ssafy/home/favorite/service/FavoriteServiceImpl.java
+++ src/main/java/com/ssafy/home/favorite/service/FavoriteServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.home.favorite.service;
 
 
+import com.ssafy.home.auth.domain.Member;
 import com.ssafy.home.estate.repository.EstateMapper;
 import com.ssafy.home.favorite.domain.Favorite;
 import com.ssafy.home.favorite.dto.FavoriteAddRequest;
@@ -25,17 +26,17 @@ public class FavoriteServiceImpl implements FavoriteService {
     private final EstateMapper estateMapper;
 
     @Override
-    public void addFavorite(FavoriteAddRequest request) {
-        if (favoriteMapper.findFavorite(request.memberId(), request.estateId()).isPresent()) {
+    public void addFavorite(Member member, FavoriteAddRequest request) {
+        if (favoriteMapper.findFavorite(member.getMid(), request.estateId()).isPresent()) {
             throw new DuplicateFavoriteException("이미 찜 목록에 존재하는 항목입니다.");
         }
 
-        favoriteMapper.insertFavorite(request.memberId(), request.estateId());
+        favoriteMapper.insertFavorite(member.getMid(), request.estateId());
     }
 
     @Override
-    public void removeFavorite(FavoriteAddRequest request) {
-        if (favoriteMapper.deleteFavorite(request.memberId(), request.estateId()) == 0) {
+    public void removeFavorite(Member member, FavoriteAddRequest request) {
+        if (favoriteMapper.deleteFavorite(member.getMid(), request.estateId()) == 0) {
             throw new DeleteFailException("삭제 실패했습니다.");
         }
     }
