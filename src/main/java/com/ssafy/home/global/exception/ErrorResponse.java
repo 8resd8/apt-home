43cfcbb30b5521d1041b45fffe5b49af72b4f553
김현우 <dkanfjgwls@naver.com>
2024-11-09 18:37:25 +0900

    reafactor: GlobalExceptionHandler 개선
    - 불명확했던 필드 개선
    status, error, message로 상태코드에 대한 설명과 메시지 필드에 대해 의미 구분을 더 명확히 했습니다.
    
    - path 필드 추가

--- src/main/java/com/ssafy/home/global/exception/ErrorResponse.java
+++ src/main/java/com/ssafy/home/global/exception/ErrorResponse.java
@@ -2,29 +2,28 @@ package com.ssafy.home.global.exception;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Builder
 @Data
 public class ErrorResponse {
-    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final LocalDateTime timestamp = LocalDateTime.now();;
     private final int status;
+    private final String error;
     private final String message;
-    private final String details;
+    private final String path;
 
-
-    public static ResponseEntity<ErrorResponse> toResponseEntity(HttpStatus httpStatus, String message) {
+    public static ResponseEntity<ErrorResponse> toResponseEntity(HttpStatus httpStatus, String message, String path) {
         return ResponseEntity
                 .status(httpStatus)
                 .body(ErrorResponse.builder()
                         .status(httpStatus.value())
-                        .message(httpStatus.name())
-                        .details(message)
+                        .error(httpStatus.name())
+                        .message(message)
+                        .path(path)
                         .build()
                 );
     }
