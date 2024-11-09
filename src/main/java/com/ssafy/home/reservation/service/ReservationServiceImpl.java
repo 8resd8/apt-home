cbbae13366f9d610829cabc120ac602525f36c53
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-09 16:03:50 +0900

    feat: 예약 생성과 동시에 예약 테이블 생성, 예외 처리 추가

--- src/main/java/com/ssafy/home/reservation/service/ReservationServiceImpl.java
+++ src/main/java/com/ssafy/home/reservation/service/ReservationServiceImpl.java
@@ -1,13 +1,15 @@
 package com.ssafy.home.reservation.service;
 
+import com.ssafy.home.auth.repository.BrokerMapper;
+import com.ssafy.home.global.enums.ReservationStatus;
 import com.ssafy.home.reservation.domain.Reservation;
-import com.ssafy.home.reservation.dto.ReservationAddRequest;
+import com.ssafy.home.reservation.dto.ReservationCreateRequest;
+import com.ssafy.home.reservation.exception.NotFoundReservation;
 import com.ssafy.home.reservation.repository.ReservationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -18,44 +20,52 @@ public class ReservationServiceImpl implements ReservationService {
     private final ReservationMapper reservationMapper;
 
     @Override
-    public void addReservation(ReservationAddRequest request) {
+    public void addReservation(ReservationCreateRequest request, String memberId) {
+        // 1. 예약 생성
         Reservation reservation = Reservation.builder()
-                .memberId(request.memberId())
+                .memberId(memberId)
                 .brokerId(request.brokerId())
                 .startTime(request.startTime())
                 .endTime(request.endTime())
                 .clientMemo(request.clientMemo())
-                .status("PENDING")
+                .brokerMemo(request.broker_memo())
+                .status(ReservationStatus.CREATED.getValue())
                 .build();
 
         reservationMapper.insertReservation(reservation);
+
+        // 2. 매물 예약 연결
+        Long reservationId = reservation.getRid();
+        request.estateIds().forEach(estateId ->
+                reservationMapper.insertReservationEstate(reservationId, estateId, request.clientMemo())
+        );
     }
 
     @Override
-    public void updateReservation(Long rid, ReservationAddRequest request) {
+    public void updateReservation(Long rid, String memberId, ReservationCreateRequest request) {
         Reservation reservation = Reservation.builder()
                 .rid(rid)
-                .memberId(request.memberId())
+                .memberId(memberId)
                 .brokerId(request.brokerId())
                 .startTime(request.startTime())
                 .endTime(request.endTime())
                 .clientMemo(request.clientMemo())
+                .brokerMemo(request.broker_memo())
+                .status(ReservationStatus.CREATED.getValue())
                 .build();
+
         reservationMapper.updateReservation(reservation);
     }
 
     @Override
-    public void deleteReservation(Long rid) {
-        reservationMapper.deleteReservation(rid);
-    }
-
-    @Override
     public Reservation getReservation(Long rid) {
-        return reservationMapper.findReservationById(rid).orElse(null);
+        return reservationMapper.findReservationById(rid).orElseThrow(() -> new NotFoundReservation(rid));
     }
 
     @Override
     public List<Reservation> getReservationsByMember(String memberId) {
-        return reservationMapper.findAllReservationsByMemberId(memberId);
+        List<Reservation> allReservation = reservationMapper.findAllReservationsByMemberId(memberId);
+        if (allReservation.isEmpty()) throw new NotFoundReservation();
+        return allReservation;
     }
 }
