0b120b09a26cdfbc32626af8a4f29b379a080b9a
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-09 21:00:45 +0900

    refactor: 불필요한 memberId 요청 제거

--- src/main/java/com/ssafy/home/favorite/dto/FavoriteAddRequest.java
+++ src/main/java/com/ssafy/home/favorite/dto/FavoriteAddRequest.java
@@ -3,7 +3,6 @@ package com.ssafy.home.favorite.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record FavoriteAddRequest(
-        @NotNull String memberId,
         @NotNull Long estateId
 ) {
 }
