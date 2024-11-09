778cbeb75f8269e3f9adb0dd18db3c87b34e7ea7
김현우 <dkanfjgwls@naver.com>
2024-11-09 18:37:25 +0900

    reafactor: GlobalExceptionHandler 개선
    - 불명확했던 필드 개선
    status, error, message로 상태코드에 대한 설명과 메시지 필드에 대해 의미 구분을 더 명확히 했습니다.
    
    - path 필드 추가

--- src/main/java/com/ssafy/home/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ssafy/home/global/exception/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.ssafy.home.global.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.NoSuchElementException;
@@ -13,12 +13,12 @@ import java.util.NoSuchElementException;
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { CustomException.class })
-    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        return ErrorResponse.toResponseEntity(e.getStatus(), e.getMessage());
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e, WebRequest request) {
+        return ErrorResponse.toResponseEntity(e.getStatus(), e.getMessage(), request.getDescription(false).replace("uri=", ""));
     }
 
     @ExceptionHandler(value = {NoSuchElementException.class})
-    protected ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
-        return ErrorResponse.toResponseEntity(HttpStatus.NOT_FOUND, e.getMessage());
+    protected ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e, WebRequest request) {
+        return ErrorResponse.toResponseEntity(HttpStatus.NOT_FOUND, e.getMessage(), request.getDescription(false).replace("uri=", ""));
     }
 }
