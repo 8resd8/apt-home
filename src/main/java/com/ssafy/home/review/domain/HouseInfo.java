7ea666c6cd15fdd9d9b3fd98ea82f642ad395684
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-10 21:17:22 +0900

    feat: 아파트 정보를 담는 엔티티

---
+++ src/main/java/com/ssafy/home/review/domain/HouseInfo.java
@@ -0,0 +1,22 @@
+package com.ssafy.home.review.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class HouseInfo {
+    private String aptSeq;      // 아파트 고유 번호
+    private String sggCd;       // 시군구 코드
+    private String umdCd;       // 읍면동 코드
+    private String umdNm;       // 읍면동 이름
+    private String jibun;       // 지번
+    private String roadNmSggCd; // 도로명 시군구 코드
+    private String roadNm;      // 도로명
+    private String roadNmBonbun;// 도로명 본번
+    private String roadNmBubun; // 도로명 부번
+    private String aptNm;       // 아파트 이름
+    private Integer buildYear;  // 건축 연도
+    private String latitude;    // 위도
+    private String longitude;   // 경도
+}
