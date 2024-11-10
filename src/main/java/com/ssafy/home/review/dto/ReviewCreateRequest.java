703945c5479ba128b1f78ec40e65b080a837f78a
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-10 16:26:34 +0900

    feat: 브로커 아이디도 요청받도록 추가

--- src/main/java/com/ssafy/home/review/dto/ReviewCreateRequest.java
+++ src/main/java/com/ssafy/home/review/dto/ReviewCreateRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record ReviewCreateRequest(
         @NotNull(message = "${required.filed}") Long reservationId,
+        @NotNull(message = "${required.filed}") Long brokerId,
         @NotNull(message = "${required.filed}") @Size(min = 1, max = 5) Integer reviewRating,
         String reviewContent
 ) {
