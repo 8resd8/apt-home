5f94cd8538e7464c1da1c882607ce39d8ef71fe7
김현우 <dkanfjgwls@naver.com>
2024-11-11 16:06:00 +0900

    refactor: 오타 수정

--- src/main/java/com/ssafy/home/reservation/exception/NotFoundReservation.java
+++ src/main/java/com/ssafy/home/reservation/exception/NotFoundReservation.java
@@ -2,7 +2,7 @@ package com.ssafy.home.reservation.exception;
 
 public class NotFoundReservation extends RuntimeException {
     public NotFoundReservation(Long rId) {
-        super(rId + "님의 예약을 찾을 수 없습니다.");
+        super(rId + "번 예약을 찾을 수 없습니다.");
     }
 
     public NotFoundReservation() {
