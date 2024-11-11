bc3064fed18c967f165822a32365aef5a776a586
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-11 23:41:34 +0900

    refactor: restful 하게 변경

--- src/main/java/com/ssafy/home/review/dto/ReviewRequest.java
+++ src/main/java/com/ssafy/home/review/dto/ReviewRequest.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record ReviewRequest(
-        @NotNull(message = "${required.filed}") Long reservationId,
         @NotNull(message = "${required.filed}") String brokerId,
         @NotNull(message = "${required.filed}") @Min(1) @Max(5) Integer reviewRating,
         String reviewContent
