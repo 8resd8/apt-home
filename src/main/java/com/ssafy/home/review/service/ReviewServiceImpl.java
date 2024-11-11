64bb5fa9fedb4a97b9b1ffa4917d66dc44fa2507
김현우 <dkanfjgwls@naver.com>
2024-11-11 17:48:30 +0900

    feat: 리뷰를 작성하기 위해 조건 (예약이 존재, 예약 완료 상태, 이미 남겨진 리뷰) 검사

--- src/main/java/com/ssafy/home/review/service/ReviewServiceImpl.java
+++ src/main/java/com/ssafy/home/review/service/ReviewServiceImpl.java
@@ -1,12 +1,16 @@
 package com.ssafy.home.review.service;
 
 import com.ssafy.home.auth.domain.Member;
+import com.ssafy.home.global.enums.ReservationStatus;
 import com.ssafy.home.global.util.PromptGenerator;
+import com.ssafy.home.reservation.domain.Reservation;
+import com.ssafy.home.reservation.exception.NotFoundReservation;
+import com.ssafy.home.reservation.repository.ReservationMapper;
 import com.ssafy.home.review.domain.Review;
 import com.ssafy.home.review.dto.ReviewAISummaryRequest;
 import com.ssafy.home.review.dto.ReviewRequest;
 import com.ssafy.home.review.exception.DuplicateReviewException;
-import com.ssafy.home.review.repository.HouseInfoMapper;
+import com.ssafy.home.review.exception.NotCompleteReviewException;
 import com.ssafy.home.review.repository.ReviewMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.model.ChatModel;
@@ -21,20 +25,12 @@ import java.util.Optional;
 public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewMapper reviewMapper;
-    private final HouseInfoMapper houseInfoMapper;
+    private final ReservationMapper reservationMapper;
     private final ChatModel chatModel;
 
     @Override
     public void addReview(Member member, ReviewRequest request) {
-        // 예약 존재 확인
-//        1Reservation reservation = reservationMapper.findById(request.reservationId());
-
-        // 예약 Status 확인 (Completed)
-
-        // 이미 남겨진 리뷰인지 확인
-        Optional<Review> findReview = reviewMapper.findReviewById(request.reservationId());
-
-        if (findReview.isEmpty()) throw new DuplicateReviewException();
+        isValidReview(request);
 
         // 리뷰 작성
         reviewMapper.insertReview(
@@ -44,6 +40,29 @@ public class ReviewServiceImpl implements ReviewService {
                 request.reviewContent(),
                 request.reviewRating()
         );
+    }
+
+    private void isValidReview(ReviewRequest request) {
+        // 1. 예약 존재 확인
+        Optional<Reservation> findReservation = reservationMapper.findReservationById(request.reservationId());
+
+        if (findReservation.isEmpty()) {
+            throw new NotFoundReservation();
+        }
+
+        Reservation reservation = findReservation.get();
+
+        // 2. 예약 Status 확인 (Completed)
+        if (!reservation.getStatus().equals(ReservationStatus.COMPLETED.toString())) {
+            throw new NotCompleteReviewException();
+        }
+
+        // 3. 이미 남겨진 리뷰인지 확인
+        Optional<Review> findReview = reviewMapper.findReviewById(request.reservationId());
+
+        if (findReview.isEmpty()) {
+            throw new DuplicateReviewException();
+        }
     }
 
     @Override
