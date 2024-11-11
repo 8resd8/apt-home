b0d984ce2e97f81ad17957cb4dafaa070fe697c8
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-11 22:13:47 +0900

    feat: 답글 생성 매퍼, 쿼리작성, broker는 값 필수로 입력

--- src/main/java/com/ssafy/home/review/repository/ReviewMapper.java
+++ src/main/java/com/ssafy/home/review/repository/ReviewMapper.java
@@ -4,6 +4,7 @@ import com.ssafy.home.review.domain.Review;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Mapper
@@ -16,5 +17,9 @@ public interface ReviewMapper {
             @Param("reviewRating") Integer reviewRating
     );
 
-    Optional<Review> findReviewById(@Param("rId") Long rId);
+    Optional<Review> findReviewById(@Param("reviewId") Long rId);
+
+    void insertReply(@Param("reviewId") Long reviewId, @Param("comment") String comment, @Param("now") LocalDateTime now);
+
+    void updateReply(@Param("reviewId") Long reviewId, @Param("comment") String comment, @Param("now") LocalDateTime now);
 }
