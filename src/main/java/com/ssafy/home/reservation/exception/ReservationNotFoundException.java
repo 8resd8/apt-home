a3e0c83ab187e3d9435eb2bcdff00eed5ca9d9fa
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-12 20:37:27 +0900

    refactor: 이름 규칙이 맞지 않는 클래스 수정

--- src/main/java/com/ssafy/home/reservation/exception/ReservationNotFoundException.java
+++ src/main/java/com/ssafy/home/reservation/exception/ReservationNotFoundException.java
@@ -1,11 +1,11 @@
 package com.ssafy.home.reservation.exception;
 
-public class ReservationNotFound extends RuntimeException {
-    public ReservationNotFound(Long rId) {
+public class ReservationNotFoundException extends RuntimeException {
+    public ReservationNotFoundException(Long rId) {
         super(rId + "번 예약을 찾을 수 없습니다.");
     }
 
-    public ReservationNotFound() {
+    public ReservationNotFoundException() {
         super("예약을 찾을 수 없습니다.");
     }
 }
