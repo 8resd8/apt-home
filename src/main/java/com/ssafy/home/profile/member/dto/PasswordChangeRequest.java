060027d886b661f6d7172afe4f977a0ccfc93344
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-12 20:58:49 +0900

    feat: 비밀번호 검증 추가

--- src/main/java/com/ssafy/home/profile/member/dto/PasswordChangeRequest.java
+++ src/main/java/com/ssafy/home/profile/member/dto/PasswordChangeRequest.java
@@ -1,8 +1,11 @@
 package com.ssafy.home.profile.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record PasswordChangeRequest(
-        String currentPassword,
-        String password1,
-        String password2
+        @NotBlank(message = "{required.field}") @Size(min = 8, message = "{size.password}") String currentPassword,
+        @NotBlank(message = "{required.field}") @Size(min = 8, message = "{size.password}") String newPassword1,
+        @NotBlank(message = "{required.field}") @Size(min = 8, message = "{size.password}") String newPassword2
 ) {
 }
