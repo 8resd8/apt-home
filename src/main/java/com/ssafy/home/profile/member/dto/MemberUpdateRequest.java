31f08cfe18df2e14f6d8c53d94e2fb3f821bd86f
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-12 20:59:10 +0900

    feat: 휴대전화 번호 검증 추가

--- src/main/java/com/ssafy/home/profile/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/ssafy/home/profile/member/dto/MemberUpdateRequest.java
@@ -1,10 +1,11 @@
 package com.ssafy.home.profile.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record MemberUpdateRequest(
         @NotBlank(message = "${required.field}") String name,
         @NotBlank(message = "${required.field}") String password,
-        String phoneNum
+        @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "{pattern.phoneNum}") String phoneNum
 ) {
 }
