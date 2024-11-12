e42d3f6e64f98f68574c1ce520942dd13a1cd9ce
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-12 22:01:59 +0900

    feat: 필드 검증 추가

--- src/main/java/com/ssafy/home/profile/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/ssafy/home/profile/member/dto/MemberUpdateRequest.java
@@ -2,10 +2,11 @@ package com.ssafy.home.profile.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record MemberUpdateRequest(
         @NotBlank(message = "${required.field}") String name,
-        @NotBlank(message = "${required.field}") String password,
+        @NotBlank(message = "{required.field}") @Size(min = 8, message = "{size.password}") String password,
         @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "{pattern.phoneNum}") String phoneNum
 ) {
 }
