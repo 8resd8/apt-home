4d5f7c412c5085a3f799e611dd37b9d14072babe
김현우 <dkanfjgwls@naver.com>
2024-11-12 17:52:55 +0900

    feat: 회원탈퇴시 사용할 비밀번호 불일치 예외

---
+++ src/main/java/com/ssafy/home/profile/member/exception/ValidPasswordException.java
@@ -0,0 +1,11 @@
+package com.ssafy.home.profile.member.exception;
+
+public class ValidPasswordException extends RuntimeException {
+    public ValidPasswordException(String message) {
+        super(message);
+    }
+
+    public ValidPasswordException() {
+        super("비밀번호가 일치하지 않습니다.");
+    }
+}
