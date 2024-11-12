10e0b170a3c231f476350f93042657f3502b8f09
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-12 22:00:33 +0900

    delete: 사용하지 않는 파일 제거

--- src/main/java/com/ssafy/home/profile/member/service/MemberProfileService.java
+++ src/main/java/com/ssafy/home/profile/member/service/MemberProfileService.java
@@ -1,21 +1,17 @@
 package com.ssafy.home.profile.member.service;
 
 import com.ssafy.home.auth.domain.Member;
+import com.ssafy.home.profile.member.dto.MemberDeleteRequest;
 import com.ssafy.home.profile.member.dto.MemberResponse;
 import com.ssafy.home.profile.member.dto.MemberUpdateRequest;
 import com.ssafy.home.profile.member.dto.PasswordChangeRequest;
-import com.ssafy.home.profile.member.dto.PasswordResetRequest;
 
 public interface MemberProfileService {
     MemberResponse findMemberById(Member memberResponse);
 
     MemberResponse updateMember(Member memberResponse, MemberUpdateRequest updateRequest);
 
-    void deleteMember(Member memberResponse, String requestPassword);
-
-    void resetPassword(PasswordResetRequest passwordResetRequest);
+    void deleteMember(Member memberResponse, MemberDeleteRequest request);
 
     void changePassword(Member member, PasswordChangeRequest passwordChangeRequest);
-
-
 }
