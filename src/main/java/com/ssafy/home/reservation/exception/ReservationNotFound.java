6dac2b5e6f8188c2f13ba0f6191574b61efd1031
김현우 <dkanfjgwls@naver.com>
2024-11-12 12:35:02 +0900

    refactor: 이름 규칙 통일

--- src/main/java/com/ssafy/home/reservation/exception/ReservationNotFound.java
+++ src/main/java/com/ssafy/home/reservation/exception/ReservationNotFound.java
@@ -1,11 +1,11 @@
 package com.ssafy.home.reservation.exception;
 
-public class NotFoundReservation extends RuntimeException {
-    public NotFoundReservation(Long rId) {
+public class ReservationNotFound extends RuntimeException {
+    public ReservationNotFound(Long rId) {
         super(rId + "번 예약을 찾을 수 없습니다.");
     }
 
-    public NotFoundReservation() {
+    public ReservationNotFound() {
         super("예약을 찾을 수 없습니다.");
     }
 }
