88c6cc5075964e0e67d85d1f39ce18d81070ca31
김현우 <dkanfjgwls@naver.com>
2024-11-12 17:59:59 +0900

    feat: 비밀번호 변경 필드

--- src/main/java/com/ssafy/home/profile/member/dto/PasswordChangeRequest.java
+++ src/main/java/com/ssafy/home/profile/member/dto/PasswordChangeRequest.java
@@ -1,8 +1,8 @@
 package com.ssafy.home.profile.member.dto;
 
 public record PasswordChangeRequest(
-        String memberId,
         String currentPassword,
-        String newPassword
+        String password1,
+        String password2
 ) {
 }
