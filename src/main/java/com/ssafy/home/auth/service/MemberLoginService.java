3b410221de5933abcaa51f8dd5ff8f0bbf495323
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-13 23:35:02 +0900

    refactor: 멤버 로그인 서비스 분리

---
+++ src/main/java/com/ssafy/home/auth/service/MemberLoginService.java
@@ -0,0 +1,33 @@
+package com.ssafy.home.auth.service;
+
+import com.ssafy.home.auth.domain.Member;
+import com.ssafy.home.auth.dto.LoginRequest;
+import com.ssafy.home.auth.dto.LoginResponse;
+import com.ssafy.home.auth.exception.LoginFailedException;
+import com.ssafy.home.auth.repository.MemberMapper;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.ssafy.home.global.enums.UserType.MEMBER;
+
+@Service
+@RequiredArgsConstructor
+public class MemberLoginService {
+
+    private final MemberMapper memberMapper;
+    private final LoginHelper loginHelper;
+    private final HttpSession session;
+
+    public LoginResponse login(LoginRequest request) {
+        Member member = memberMapper.findById(request.id()).orElseThrow(LoginFailedException::new);
+        loginHelper.checkPassword(member.getPassword(), request.password(), member.getSalt());
+        loginHelper.setSessionAttribute(request.id(), MEMBER);
+
+        member.updateLastLogin();
+        memberMapper.updateLastLogin(member.getMid(), member.getLastLogin());
+
+        return new LoginResponse(member.getMid(), member.getName(), member.getEmail(), MEMBER.name(), session.getId());
+    }
+
+}
