e1c8f55f7edc277d3d55530eb811f125f7830cd4
김현우 <dkanfjgwls@naver.com>
2024-11-13 12:28:39 +0900

    feat: 이메일 제목

---
+++ src/main/java/com/ssafy/home/global/enums/Email.java
@@ -0,0 +1,13 @@
+package com.ssafy.home.global.enums;
+
+public enum Email {
+    TITLE("SSAFY FINAL-PROJECT 이메일 인증 코드");
+
+
+
+    private final String value;
+
+    Email(String value) {
+        this.value = value;
+    }
+}
