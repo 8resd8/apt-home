536d3a69d3aa99528105457f6df31046534e17f0
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-13 23:34:37 +0900

    refactor: 멤버 회원가입 서비스 분리

---
+++ src/main/java/com/ssafy/home/auth/service/MemberSignUpService.java
@@ -0,0 +1,30 @@
+package com.ssafy.home.auth.service;
+
+import com.ssafy.home.auth.domain.Member;
+import com.ssafy.home.auth.dto.MemberSignUpRequest;
+import com.ssafy.home.auth.dto.SignUpResponse;
+import com.ssafy.home.auth.repository.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberSignUpService {
+
+    private final MemberMapper memberMapper;
+    private final SignUpHelper signUpHelper;
+
+    public SignUpResponse signUp(MemberSignUpRequest request) {
+        signUpHelper.checkDuplicatedId(request.id());
+
+        String salt = signUpHelper.generateSalt();
+        String hashedPassword = signUpHelper.hashPassword(request.password(), salt);
+
+        Member member = Member.toEntity(request.id(), hashedPassword, salt, request.email(), request.phoneNum(), request.name());
+        memberMapper.insertMember(member);
+
+        return new SignUpResponse(request.id(), request.email());
+    }
+
+
+}
