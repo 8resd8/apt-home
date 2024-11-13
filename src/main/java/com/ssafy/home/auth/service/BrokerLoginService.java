f4d0fdec87e406f9155d06c51f0973cc8def0bb9
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-13 23:34:52 +0900

    refactor: 브로커 로그인 서비스 분리

---
+++ src/main/java/com/ssafy/home/auth/service/BrokerLoginService.java
@@ -0,0 +1,34 @@
+package com.ssafy.home.auth.service;
+
+import com.ssafy.home.auth.domain.Broker;
+import com.ssafy.home.auth.dto.LoginRequest;
+import com.ssafy.home.auth.dto.LoginResponse;
+import com.ssafy.home.auth.exception.LoginFailedException;
+import com.ssafy.home.auth.repository.BrokerMapper;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.ssafy.home.global.enums.UserType.BROKER;
+
+@Service
+@RequiredArgsConstructor
+public class BrokerLoginService {
+
+    private final BrokerMapper brokerMapper;
+    private final LoginHelper loginHelper;
+    private final HttpSession session;
+
+    public LoginResponse login(LoginRequest request) {
+        Broker broker = brokerMapper.findById(request.id()).orElseThrow(LoginFailedException::new);
+
+        broker.updateLastLogin();
+        brokerMapper.updateLastLogin(broker.getBid(), broker.getLastLogin());
+
+        loginHelper.checkPassword(broker.getPassword(), request.password(), broker.getSalt());
+        loginHelper.setSessionAttribute(broker.getBid(), BROKER);
+
+        return new LoginResponse(broker.getBid(), broker.getBrokerName(), broker.getEmail(), BROKER.name(), session.getId());
+    }
+
+}
