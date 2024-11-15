2a84f35bd834c44bd27d330a8df6aa2d3761a4b1
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-15 20:57:50 +0900

    feat: 이미지 접근 경로 외부에서 설정, 메서드 분리

--- src/main/java/com/ssafy/home/auth/service/signup/FileStorageService.java
+++ src/main/java/com/ssafy/home/auth/service/signup/FileStorageService.java
@@ -15,6 +15,9 @@ public class FileStorageService {
     @Value("${file.upload.dir}")
     private String uploadPath;
 
+    @Value("${upload.access.url}")
+    private String uploadAccessUrl;
+
     public String uploadProfileImage(MultipartFile profileImage) {
         String uploadDirPath = new File(uploadPath).getAbsolutePath();
         String fileName = UUID.randomUUID() + "_" + profileImage.getOriginalFilename(); // 고유한 파일 이름 생성
@@ -24,13 +27,20 @@ public class FileStorageService {
             directory.mkdirs();
         }
 
-        File saveFile = new File(uploadDirPath + "/" + fileName); // 파일이 저장되는 곳
+        File saveFile = new File(uploadDirPath + File.separator + fileName); // 파일이 저장되는 곳
         try {
             profileImage.transferTo(saveFile);
         } catch (IOException e) {
             throw new FileUploadException("파일 업로드 실패: " + profileImage.getOriginalFilename(), e);
         }
 
-        return "/upload/profile_images/" + fileName; // 클라이언트가 접속하는 URL
+        return uploadAccessUrl + fileName; // 클라이언트가 접속할 수 있는 URL
+    }
+
+    public String getImageUrl(MultipartFile profileImage) {
+        if (profileImage == null || !profileImage.isEmpty()) {
+            return null;
+        }
+        return uploadProfileImage(profileImage);
     }
 }
