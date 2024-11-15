fa6ea838ceca67a142d798f9d856cc614e112450
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-15 18:57:35 +0900

    feat: 회원가입시 파일저장 서비스 로직 구현

---
+++ src/main/java/com/ssafy/home/auth/service/signup/FileStorageService.java
@@ -0,0 +1,36 @@
+package com.ssafy.home.auth.service.signup;
+
+import com.ssafy.home.auth.exception.FileUploadException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class FileStorageService {
+
+    @Value("${file.upload.dir}")
+    private String uploadPath;
+
+    public String uploadProfileImage(MultipartFile profileImage) {
+        String uploadDirPath = new File(uploadPath).getAbsolutePath();
+        String fileName = UUID.randomUUID() + "_" + profileImage.getOriginalFilename(); // 고유한 파일 이름 생성
+
+        File directory = new File(uploadDirPath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        File saveFile = new File(uploadDirPath + "/" + fileName); // 파일이 저장되는 곳
+        try {
+            profileImage.transferTo(saveFile);
+        } catch (IOException e) {
+            throw new FileUploadException("파일 업로드 실패: " + profileImage.getOriginalFilename(), e);
+        }
+
+        return "/upload/profile_images/" + fileName; // 클라이언트가 접속하는 URL
+    }
+}
