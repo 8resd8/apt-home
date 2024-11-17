edd4dbc22cc35bce0cdc51865c65fe282f23276a
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-17 11:25:11 +0900

    feat: 연결안된 aws 포함시키지 않는 설정

--- src/main/java/com/ssafy/home/HomeApplication.java
+++ src/main/java/com/ssafy/home/HomeApplication.java
@@ -1,10 +1,11 @@
 package com.ssafy.home;
 
+import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {S3AutoConfiguration.class})
 @EnableAspectJAutoProxy
 public class HomeApplication {
 
