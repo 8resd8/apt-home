dbf0cc6344f5a4d307a7799dad309ed88de57575
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-18 23:58:20 +0900

    feat: 기본 컨트롤러 뼈대

---
+++ src/main/java/com/ssafy/home/info/controller/ChartController.java
@@ -0,0 +1,14 @@
+package com.ssafy.home.info.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chart")
+@RequiredArgsConstructor
+public class ChartController {
+
+
+}
