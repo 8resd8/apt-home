1cb53b8b8f72f3634c57d1ae1582f36f0aa70f78
김현우 <dkanfjgwls@naver.com>
2024-11-19 17:30:39 +0900

    feat: 차트 평균가격 클래스

--- src/main/java/com/ssafy/home/info/dto/AveragePrice.java
+++ src/main/java/com/ssafy/home/info/dto/AveragePrice.java
@@ -1,12 +1,8 @@
 package com.ssafy.home.info.dto;
 
-import lombok.*;
-
-@Getter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
-public class AveragePrice {
-    private String month;
-    private Long averageAmount;
+public record AveragePrice(
+        String year,
+        String month,
+        Long averageAmount
+) {
 }
