115689a1c626c16783e0b4c9cdde55fbcca86ffa
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-20 22:30:57 +0900

    feat: 이미지 검증 로직 추가

--- src/main/java/com/ssafy/home/auth/service/signup/S3StorageService.java
+++ src/main/java/com/ssafy/home/auth/service/signup/S3StorageService.java
@@ -10,12 +10,21 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class S3StorageService implements StorageService {
 
+    // 허용할 파일 확장자
+    private static final Set<String> ALLOWED_EXTENSIONS = Stream.of("jpg", "jpeg", "png")
+            .collect(Collectors.toSet());
+
+    // 허용할 MIME 타입
+    private static final Set<String> ALLOWED_MIME_TYPES = Set.of("image/jpeg", "image/png", "image/webp");
     private final S3Client s3Client;
 
     @Value("${aws.s3.bucket}")
@@ -30,6 +39,8 @@ public class S3StorageService implements StorageService {
         if (file == null || file.isEmpty()) {
             return null;
         }
+
+        validateFile(file);
 
         String fileName = "upload/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
 
@@ -46,5 +57,22 @@ public class S3StorageService implements StorageService {
         }
 
         return uploadAccessUrl + fileName; // 클라이언트가 접속할 수 있는 URL
+    }
+
+    void validateFile(MultipartFile file) {
+        int extension = file.getOriginalFilename().lastIndexOf('.');
+        if (extension == -1) {
+            throw new FileUploadException("파일 확장자가 없습니다.");
+        }
+
+        String mimeType = file.getContentType();
+        if (mimeType == null || !ALLOWED_MIME_TYPES.contains(mimeType.toLowerCase())) {
+            throw new FileUploadException("ContentType 확인하세요");
+        }
+
+        String fileExtension = file.getOriginalFilename().substring(extension + 1).toLowerCase(); // 확장자만 확인
+        if (!ALLOWED_EXTENSIONS.contains(fileExtension)) {
+            throw new FileUploadException("허용되지 않는 파일 형식입니다. (허용: " + ALLOWED_EXTENSIONS + ")");
+        }
     }
 }
