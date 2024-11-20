948ab770f098165e0589835c902bb73c2cfab26f
RESD\dkanf <dkanfjgwls@naver.com>
2024-11-20 22:30:45 +0900

    feat: 현재 클래스에 테이블 필드가 없는 컬럼 추가

--- src/main/java/com/ssafy/home/estate/dto/Estate.java
+++ src/main/java/com/ssafy/home/estate/dto/Estate.java
@@ -3,10 +3,13 @@ package com.ssafy.home.estate.dto;
 import com.ssafy.home.global.enums.EstateType;
 import lombok.Getter;
 
+import java.sql.Timestamp;
+
 @Getter
 public class Estate {
     Long eid;
     String brokerId;
+    String aptSeq;
     EstateType type;
     String status;
     Integer floor;
@@ -14,5 +17,7 @@ public class Estate {
     Double area;
     String amount;
     String desc;
+    Timestamp createdAt;
+    Timestamp updatedAt;
     String estateImage;
 }
