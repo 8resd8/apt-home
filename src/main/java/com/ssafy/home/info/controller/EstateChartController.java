f6042bd5cb87f40b01284eb3e7379a1ddabb9e10
김현우 <dkanfjgwls@naver.com>
2024-11-20 12:01:37 +0900

    refactor: 허용한 경로 이미 있는걸로 통일

--- src/main/java/com/ssafy/home/info/controller/EstateChartController.java
+++ src/main/java/com/ssafy/home/info/controller/EstateChartController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/estate")
+@RequestMapping("/estate")
 @RequiredArgsConstructor
 public class EstateChartController {
 
