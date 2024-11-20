46b2b73ad2f794e14d2291f217c655056f013a2a
김현우 <dkanfjgwls@naver.com>
2024-11-20 16:46:12 +0900

    feat: S3 비활성화 해제

--- src/main/java/com/ssafy/home/HomeApplication.java
+++ src/main/java/com/ssafy/home/HomeApplication.java
@@ -1,11 +1,10 @@
 package com.ssafy.home;
 
-import io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication(exclude = {S3AutoConfiguration.class})
+@SpringBootApplication
 @EnableAspectJAutoProxy
 public class HomeApplication {
 
