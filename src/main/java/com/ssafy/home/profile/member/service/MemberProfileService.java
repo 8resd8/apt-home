1b751dfdd2eb8d835c7ca8a575d0b965308f45be
김현우 <dkanfjgwls@naver.com>
2024-11-21 13:58:49 +0900

    feat: 이미지가 있는 경우 수정 구현

--- src/main/java/com/ssafy/home/profile/member/service/MemberProfileService.java
+++ src/main/java/com/ssafy/home/profile/member/service/MemberProfileService.java
@@ -2,11 +2,12 @@ package com.ssafy.home.profile.member.service;
 
 import com.ssafy.home.auth.domain.Member;
 import com.ssafy.home.profile.member.dto.*;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface MemberProfileService {
     MemberResponse findMemberById(Member memberResponse);
 
-    MemberResponse updateMember(Member memberResponse, MemberUpdateRequest updateRequest);
+    MemberResponse updateMember(Member memberResponse, MemberUpdateRequest updateRequest, MultipartFile image);
 
     void deleteMember(Member memberResponse, MemberDeleteRequest request);
 
