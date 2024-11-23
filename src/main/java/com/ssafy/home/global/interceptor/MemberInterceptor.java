3edee261eb4ac861a078e8982376d0ebd688d950
김현우 <dkanfjgwls@naver.com>
2024-11-23 23:46:32 +0900

    fix: preflight err 수정

--- src/main/java/com/ssafy/home/global/interceptor/MemberInterceptor.java
+++ src/main/java/com/ssafy/home/global/interceptor/MemberInterceptor.java
@@ -5,6 +5,7 @@ import com.ssafy.home.global.enums.UserType;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -13,6 +14,11 @@ public class MemberInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // preflight 응답
+        if(HttpMethod.OPTIONS.name().equals(request.getMethod())) {
+            return true;
+        }
+
         HttpSession session = request.getSession(false);
 
         String type = (String) session.getAttribute(Session.TYPE.name());
