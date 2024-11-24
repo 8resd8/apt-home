bff54da2b821f3fd3ba0df19ff4ea3d3512be2f0
김현우 <dkanfjgwls@naver.com>
2024-11-24 16:20:55 +0900

    refactor: 불필요한 어노테이션 제거

--- src/main/java/com/ssafy/home/info/dto/RegionCodeResponse.java
+++ src/main/java/com/ssafy/home/info/dto/RegionCodeResponse.java
@@ -1,14 +1,8 @@
 package com.ssafy.home.info.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Setter
 public class RegionCodeResponse {
     private String code;
     private String name;
