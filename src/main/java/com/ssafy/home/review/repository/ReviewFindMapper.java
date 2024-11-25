f10d011735144edadddb99b9fd7c44977ad9df7b
김현우 <dkanfjgwls@naver.com>
2024-11-25 17:01:21 +0900

    feat: 리뷰 통계 제공하는 쿼리, 매퍼

--- src/main/java/com/ssafy/home/review/repository/ReviewFindMapper.java
+++ src/main/java/com/ssafy/home/review/repository/ReviewFindMapper.java
@@ -1,6 +1,7 @@
 package com.ssafy.home.review.repository;
 
 import com.ssafy.home.review.dto.ReviewResponse;
+import com.ssafy.home.review.dto.ReviewScoreResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,4 +13,6 @@ public interface ReviewFindMapper {
     List<ReviewResponse> findAllMemberReview(@Param("memberId") String memberId);
 
     List<ReviewResponse> findAllBrokerReview(@Param("brokerId") String brokerId);
+
+    ReviewScoreResponse findReviewScore(@Param("brokerId") String brokerId);
 }
