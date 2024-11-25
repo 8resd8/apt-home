1271c27fe1df383863b34daf147c6f58afad01ae
김현우 <dkanfjgwls@naver.com>
2024-11-25 17:31:11 +0900

    fix: 리뷰 합 -> 리뷰 횟수로 변경

--- src/main/java/com/ssafy/home/review/dto/ReviewScoreResponse.java
+++ src/main/java/com/ssafy/home/review/dto/ReviewScoreResponse.java
@@ -2,7 +2,7 @@ package com.ssafy.home.review.dto;
 
 public record ReviewScoreResponse(
         Double reviewAvg,
-        Long reviewTotal,
+        Long reviewCnt,
         Long reviewUnComment
 ) {
 }
